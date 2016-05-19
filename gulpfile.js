e1f1c7008f9af5e3bc05eb0c542678daf81d4f52
kobezzza <kobezzza@gmail.com>
2016-05-19 13:07:04 +0300

    Improved build

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,32 @@
 const
 	gulp = require('gulp'),
 	typograf = require('gulp-typograf'),
+	markdown = require('gulp-markdown-it'),
+	replace = require('gulp-replace'),
 	run = require('gulp-run');
 
-gulp.task('yaspeller', () =>
-	run('node node_modules/yaspeller/bin/yaspeller ./').exec());
+function error() {
+	return (err) => {
+		console.error(err.message);
+	};
+}
 
-gulp.task('typograf', () => {
-	gulp.src('./**/*_ru.md')
-		.pipe(typograf({lang: 'ru'}))
-		.pipe(gulp.dest('./'));
+gulp.task('yaspeller', (cb) => {
+	run('yaspeller ./').exec()
+		.on('error', error())
+		.on('finish', cb);
 });
 
-gulp.task('watch', () => gulp.watch('./**/*_ru.md', ['typograf']));
-gulp.task('default', ['typograf', 'watch']);
+gulp.task('typograf', (cb) => {
+	gulp.src('./*.md')
+		.pipe(markdown({options: {highlight: (str, lang) => `<source lang="${lang || 'js'}">${str}</source>`}}))
+		.on('error', error())
+		.pipe(replace(/<pre><code\s*.*?>|<\/code><\/pre>/g, ''))
+		.pipe(typograf({lang: 'ru'}))
+		.on('error', error())
+		.pipe(gulp.dest('./'))
+		.on('end', cb);
+});
+
+gulp.task('watch', () => gulp.watch('./*.md', ['yaspeller', 'typograf']));
+gulp.task('default', ['yaspeller', 'typograf', 'watch']);
